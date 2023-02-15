270fc987f685d85b9691550eab03702868a8d750
mydmk7874 <mydmk7874@gmail.com>
2023-02-15 23:48:23 +0900

    과제 Lv2 BlogListResponseDto 수정, 정상작동 확인

--- src/main/java/com/sparta/springpracblog/dto/BlogListResponseDto.java
+++ src/main/java/com/sparta/springpracblog/dto/BlogListResponseDto.java
@@ -1,11 +1,17 @@
 package com.sparta.springpracblog.dto;
 
 import com.sparta.springpracblog.entity.Blog;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@RequiredArgsConstructor
 public class BlogListResponseDto {
-    private List<BlogResponseDto> blogList;
+    private List<BlogResponseDto> blogList = new ArrayList<>();
 
     public void addBlog(BlogResponseDto responseDto) {
         blogList.add(responseDto);
